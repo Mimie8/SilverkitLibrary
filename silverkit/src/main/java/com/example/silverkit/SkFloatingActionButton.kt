9f1439f2a35e3906c71e36c443281fe4028ca8ef
Mimie8 <amelie.dieudonne@student.unamur.be>
2021-03-17 17:47:04 +0100

    getClicks test

---
+++ silverkit/src/main/java/com/example/silverkit/SkFloatingActionButton.kt
@@ -0,0 +1,16 @@
+package com.example.silverkit
+
+import android.content.Context
+import android.util.AttributeSet
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+
+class SkFloatingActionButton : FloatingActionButton{
+
+    constructor(context : Context): super(context)
+    constructor(context : Context, attributeSet: AttributeSet): super(context, attributeSet)
+    constructor(context : Context, attributeSet: AttributeSet, defStyleAttr: Int): super(context, attributeSet, defStyleAttr)
+
+    fun adjustSize(){
+        //Adjust floating button size
+    }
+}
