3748eb70966bfd86e9fde39a7b3a8e711aef91d9
Mimie8 <amelie.dieudonne@student.unamur.be>
2021-03-26 14:04:29 +0100

    Save csv file

--- silverkit/src/main/java/com/amelie/silverkit/SkTools.kt
+++ silverkit/src/main/java/com/amelie/silverkit/SkTools.kt
@@ -3,30 +3,38 @@ package com.amelie.silverkit
 import android.util.Log
 import android.view.MotionEvent
 import android.view.View
+import com.amelie.silverkit.datamanager.SkOnTouchData
 import com.amelie.silverkit.widgets.SkFloatingActionButton
 import com.amelie.silverkit.widgets.SkRecyclerView
+import java.io.FileWriter
+import java.io.IOException
+import java.nio.file.Files
+import java.nio.file.Paths
 import java.sql.Timestamp
-import java.time.LocalDate
-import java.time.format.DateTimeFormatter
 
 interface SkTools {
 
     enum class ViewType {
-        FAB, RV, NONE
+        NONE, FAB, RV
     }
 
     fun toolOnTouch(view: View, event: MotionEvent) {
 
-        val pressure = event.getPressure()
-        val rawX = event.getRawX()
-        val rawY = event.getRawY()
-        val timestamp = Timestamp(System.currentTimeMillis())
-
-
         if(event.getAction() == MotionEvent.ACTION_DOWN){
             //PRESS ON THE VIEW
+
             val viewType = getViewType(view)
+            val viewLocal = getViewLocal(view)
+            val pressure = event.getPressure()
+            val rawX = event.getRawX()
+            val rawY = event.getRawY()
+            val timestamp = Timestamp(System.currentTimeMillis())
+
+            val touchData = SkOnTouchData(viewType, viewLocal, pressure, rawX, rawY, timestamp)
+
             Log.d("info", "SILVERKIT TOOL ONTOUCH : VIEW = $viewType | X = $rawX | Y = $rawY | PRESSURE = $pressure | TIMESTAMP : $timestamp")
+
+            saveOnTouchData(touchData)
         }
 
     }
@@ -43,5 +51,53 @@ interface SkTools {
         }
 
     }
+
+    private fun getViewLocal(view: View) : String {
+        return view.context.toString()
+    }
+
+    private fun saveOnTouchData(touchData: SkOnTouchData){
+
+        var fileWriter: FileWriter? = null
+        try {
+            fileWriter = FileWriter("FileOnTouchData.csv")
+
+            /*
+            fileWriter.append(CSV_HEADER)
+            fileWriter.append('\n')
+             */
+
+            fileWriter.append(touchData.viewType.toString())
+            fileWriter.append(',')
+            fileWriter.append(touchData.viewLocal.toString())
+            fileWriter.append(',')
+            fileWriter.append(touchData.pressure.toString())
+            fileWriter.append(',')
+            fileWriter.append(touchData.rawX.toString())
+            fileWriter.append(',')
+            fileWriter.append(touchData.rawY.toString())
+            fileWriter.append(',')
+            fileWriter.append(touchData.timestamp.toString())
+            fileWriter.append('\n')
+
+            println("Write CSV successfully!")
+            Log.d("info", "SILVERKIT TOOL ONTOUCH : Write CSV successfully!)")
+
+        } catch (e: Exception) {
+            Log.d("info", "SILVERKIT TOOL ONTOUCH : Writing CSV error!)")
+            e.printStackTrace()
+        } finally {
+            try {
+                fileWriter!!.flush()
+                fileWriter.close()
+            } catch (e: IOException) {
+                Log.d("info", "SILVERKIT TOOL ONTOUCH : Flushing/closing error!)")
+                e.printStackTrace()
+            }
+        }
+
+    }
+
+
 
 }
---
+++ silverkit/src/main/java/com/amelie/silverkit/datamanager/SkOnTouchData.kt
@@ -0,0 +1,29 @@
+package com.amelie.silverkit.datamanager
+
+import com.amelie.silverkit.SkTools
+import java.sql.Timestamp
+
+class SkOnTouchData {
+
+    var viewType : SkTools.ViewType? = null
+    var viewLocal : String? = null
+    var pressure: Float = 0f
+    var rawX: Float = 0f
+    var rawY: Float = 0f
+    var timestamp : Timestamp? = null
+
+    constructor()
+    constructor(viewType: SkTools.ViewType, viewLocal: String, pressure: Float, rawX: Float, raxY: Float, timestamp: Timestamp){
+        this.viewType = viewType
+        this.viewLocal = viewLocal
+        this.pressure = pressure
+        this.rawX = rawX
+        this.rawY = rawY
+        this.timestamp = timestamp
+    }
+
+    override fun toString(): String {
+        return "SkOnTouchData(viewType=$viewType, viewLocal=$viewLocal, pressure=$pressure, rawX=$rawX, rawY=$rawY, timestamp=$timestamp)"
+    }
+
+}
